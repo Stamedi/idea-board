0c964f07ad31c0f9d41fb542009685c55fc68916
Mario Arro <marioarro186@gmail.com>
2022-11-19 13:30:47 +0200

    Create tiles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
+  const [tileArr, setTileArr] = useState([]);
+
+  const createTile = () => {
+    const time = new Date()
+    setTileArr([...tileArr, { id:uuidv4(), title:'New Title', description:'New Desc', time:time.toLocaleString() }])
+  }
+
+  const editTile = () => {
+
+  }
   return (
     <div className="App">
-      <h1>Idea Board</h1>
+      <h1>IDEA BOARD</h1>
+      <button onClick={() => createTile()}>Create New Idea</button>
+        {tileArr.map((tile) => (
+          <div key={tile.id}>
+            <h1>{tile.id}</h1>
+            <input className="title" onChange={(e) => e.target.value}placeholder="Title"></input>
+            <textarea className="description" placeholder="Description" onChange={(e) => e.target.value}></textarea>
+            <div>
+              <p>{tile.time}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
