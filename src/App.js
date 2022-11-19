25914d8747d88d30b5afd1689bbef7edac21be65
Mario Arro <marioarro186@gmail.com>
2022-11-19 14:17:27 +0200

    remove btn func

--- src/App.js
+++ src/App.js
@@ -7,27 +7,34 @@ function App() {
 
   const createTile = () => {
     const time = new Date()
-    setTileArr([...tileArr, { id:uuidv4(), title:'New Title', description:'New Desc', time:time.toLocaleString() }])
+    setTileArr([{ id:uuidv4(), title:'New Title', description:'New Desc', time:time.toLocaleString() },...tileArr])
   }
 
   const editTile = () => {
 
   }
+
+  const removeTile = (id) => {
+    setTileArr(tileArr.filter((tile) => tile.id !== id ))
+  }
   return (
     <div className="App">
       <h1>IDEA BOARD</h1>
       <button onClick={() => createTile()}>Create New Idea</button>
-        <div className="tiles">
-          {tileArr.map((tile) => (
-            <div className="tile" key={tile.id}>
-              <input className="tile-title" onChange={(e) => e.target.value}placeholder="Title"></input>
-              <textarea
-              maxlength="140" className="tile-description" placeholder="Description" onChange={(e) => e.target.value}></textarea>
-              <div>
-                <p>{tile.time}</p>
+        <div className="flex-container">
+          <div className="tiles">
+            {tileArr.map((tile) => (
+              <div className="tile" key={tile.id}>
+                <input className="tile-title" onChange={(e) => e.target.value}placeholder="Title"></input>
+                <textarea
+                maxLength="140" className="tile-description" placeholder="Description" onChange={(e) => e.target.value}></textarea>
+                <div className="tile-bottom-cont">
+                  <p>{tile.time}</p>
+                  <button onClick={() => removeTile(tile.id)}>Remove</button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
     </div>
   );
