3c5127524e8fdc0fd867096a0b281b503036969d
Mario Arro <marioarro186@gmail.com>
2022-11-20 15:19:51 +0200

    local storage updates finished

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
-  const [tileArr, setTileArr] = useState([]);
+  const [tileArr, setTileArr] = useState('');
 
   const createTile = () => {
     const time = new Date()
      setTileArr([{ id:uuidv4(), title:'', description:'', time:time.toLocaleString() },...tileArr])
   }
 
-  const editTitle = (id, title) => {
+  const editTitle = (id, e) => {
     const time = new Date()
 
     const newState = tileArr.map((tile) => {
       if (tile.id === id) {
-        return {...tile, title, time:time.toLocaleString() }
+        return {...tile, title:e, time:time.toLocaleString() }
       }
       return tile
     })
     setTileArr(newState)
   }
 
-  const editDesc = (id, description) => {
+  const editDesc = (id, e) => {
     const time = new Date()
 
     const newState = tileArr.map((tile) => {
       if (tile.id === id) {
-        return {...tile, description, time:time.toLocaleString() }
+        return {...tile, description:e, time:time.toLocaleString() }
       }
       return tile
     })
@@ -39,18 +39,18 @@ function App() {
   }
 
   useEffect(() => {
-    if (tileArr.length !== 0) {
+    if (typeof tileArr !== "string") {
       window.localStorage.setItem('notes', JSON.stringify(tileArr));
-    } 
+    }
   }, [createTile, editTitle, editDesc, removeTile])
-
   
   useEffect(() => {
     if (window.localStorage.getItem('notes') !== null) {
-      if (window.localStorage.getItem('notes').length !== 0)
-      setTileArr(JSON.parse(window.localStorage.getItem('notes')))
-    }
+        setTileArr(JSON.parse(window.localStorage.getItem('notes')))
+    } 
 },[])
+
+
 
   
   return (
@@ -61,9 +61,9 @@ function App() {
           <div className="tiles">
             {tileArr.length !== 0 && (tileArr.map((tile) => (
               <div className="tile" key={tile.id}>
-                <input className="tile-title" onChange={() => editTitle(tile.id, tile.title)}placeholder="Title" autoFocus></input>
+                <input onChange={(e) => editTitle(tile.id, e.target.value)}type="text" className="tile-title" placeholder="Title" value={tile.title} autoFocus />
                 <textarea
-                maxLength="140" className="tile-description" placeholder="Description" onChange={() => editDesc(tile.id, tile.description)}></textarea>
+                maxLength="140" className="tile-description" placeholder="Description" onChange={(e) => editDesc(tile.id, e.target.value)} value={tile.description}></textarea>
                 <div className="tile-bottom-cont">
                   <p>{tile.time}</p>
                   <button onClick={() => removeTile(tile.id)}>Remove</button>
